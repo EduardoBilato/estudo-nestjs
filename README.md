98beebef42ef04a860d4d1d694644d01f1b70d4c
EduardoBilato <eduardobilato@gmail.com>
2022-04-17 11:11:13 -0300

    feat: estudo nest

---
+++ README.md
@@ -0,0 +1,17 @@
+## Estudo de nestjs
+
+## Sobre o repositório
+Esse repositório contém o código-fonte ministrado na aula Intensivão com Nest.js: [https://www.youtube.com/watch?v=ucHzhctPlbA](https://www.youtube.com/watch?v=ucHzhctPlbA)
+
+## Rodar a aplicação
+
+Execute os comandos:
+
+```bash
+docker-compose up
+```
+Alternativa 1:
+  Acesse no browser http://localhost:3000/api. Documentação em swagger.
+
+Alternativa 2:
+  Acesse no browser http://localhost:3000/orders. Use o arquivo `api.http` para testar a API.
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Esquenta Imersão 7.0 API Rest')
+    .setDescription('Estudo de NestJS')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+}
+bootstrap();
